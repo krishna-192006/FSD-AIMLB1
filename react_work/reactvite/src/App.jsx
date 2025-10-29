5901a3b99f5674e062d074387daa10b7334059af
Krishna <your_email@example.com>
2025-10-29 14:50:55 +0530

    react

---
+++ react_work/reactvite/src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Profile from './profile'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <div className='conatainer'>
+      <p>Welcome to vite</p>
+      <Profile />
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ react_work/reactvite/src/profile.jsx
@@ -0,0 +1,20 @@
+const Profile = () => {
+    return ( 
+        <>
+        <div className="parent">
+            <h2>Welcome to my profile</h2>
+        </div>
+        <img src="">
+        </img>
+        <div>
+            <h2>Name:Krishna</h2>
+            <h2>Roll Number:2300321530105</h2>
+            <h3>Branch:CSE AIML</h3>
+            <h3>Section:B</h3>
+            <h2>College:ABES</h2>
+        </div>
+        </>
+     );
+}
+ 
+export default Profile;
