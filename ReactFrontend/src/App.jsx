c04194b89a252ab8f89f960ed36c7246203176d9
Krishna <your_email@example.com>
2026-02-18 14:16:17 +0530

    react

--- ReactFrontend/src/App.jsx
+++ ReactFrontend/src/App.jsx
@@ -1,19 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-  function getData(){
-    alert("Hi get Inside!!")
+  const [data, setData] = useState([]);
+  async function getData() {
+    // alert("Hello from App.jsx");
+    const response = await fetch("http://localhost:4007/data");
+    const jsondata = await response.json();
+    setData(jsondata.msg);
   }
-
   return (
-   <>
-   <p> WELCOME TO REACT APP</p>
-   <button onClick={getData}>Fetch data</button>
-   </>
-  )
+    <>
+      <div className="div">
+        <h2>Hello from App.jsx</h2>
+        {JSON.stringify(data)}
+        <button onClick={getData}>FetchData</button>
+      </div>
+    </>
+  );
 }
 
-export default App
+export default App;
--- nodebackend/index.js
+++ nodebackend/index.js
@@ -1,25 +1,37 @@
 const http = require("http");
+const sum = require("./fetchData.js");
 const PORT = 4007;
-const server = http.createServer((req, res)=>{
-//     res.setHeader('content-type','text/html');
-// res.end("HELOO !!")
-if(req.url=='/msg' && req.method =='GET'){
-    res.setHeader('content-type','text/html');
-    res.end("<h2>Welcome message from node server </h2>")
-}
+const server = http.createServer( async(req, res) => {
 
-if(req.url=='/data' && req.method =='GET'){
-    res.setHeader('content-type','application/json');
-    res.end(JSON.stringify({msg:"This is json data"}))
-}
-if(req.url=='/data' && req.method== "POST"){
-    res.setHeader('Content-Type','application/json')
-    res.end(JSON.stringify({msg:"Post method for data insertion"}));
-}
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5173');
+  res.setHeader('Access-Control-Allow-Methods', 'GET,POST,DELETE,PUT');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
 
+  // res.setHeader('content-type','text/html')
+  // res.end("<h2 style=color:red> Hello Welcome to Node Server </h2>");
+
+  if (req.url == "/msg" && req.method == "GET") {
+    res.setHeader("content-type", "text/html");
+    res.end("<h2 style=color:red> Hello Welcome to Node Server </h2>");
+  }
+
+  if (req.url == "/data" && req.method == "GET") {
+    res.setHeader("content-type", "application/json");
+    const sumData = await sum()
+    res.end(JSON.stringify({ msg: sumData }));
+  }
+
+  if (req.url == "/data" && req.method == "POST") {
+    res.setHeader("content-type", "application/json");
+    res.end(JSON.stringify({ msg: "This is JSON POST data" }));
+  }
+
+  if (req.url == "/data" && req.method == "DELETE") {
+    res.setHeader("content-type", "application/json");
+    res.end(JSON.stringify({ msg: "This is JSON delete data" }));
+  }
 });
 
-server.listen(PORT, ()=>{
-    console.log('Service is available at ${PORT}')
-})
-
+server.listen(PORT, () => {
+  console.log(`Service is availabe at ${PORT}`);
+});
